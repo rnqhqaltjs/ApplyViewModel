3c29a73af470d82c15a0a3106045be305cb2b9f2
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-07-13 21:45:46 +0900

    DataBinding

--- app/src/main/java/com/example/applyviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/applyviewmodel/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.applyviewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.applyviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+//    private val binding: ActivityMainBinding by lazy {
+//        ActivityMainBinding.inflate(layoutInflater)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+//        setContentView(binding.root)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
 
 //        var counter = 100
 //        binding.textView.text = counter.toString()
@@ -32,20 +34,30 @@ class MainActivity : AppCompatActivity() {
 //            binding.textView.text = myViewModel.counter.toString()
 //        }
 
-        val factory = MyViewModelFactory(100,this)
+        //팩토리 패턴을 통해 뷰모델에 초기값 적용
+        val factory = MyViewModelFactory(10,this)
 //        val myViewModel = ViewModelProvider(this,factory)[MyViewModel::class.java]
         val myViewModel by viewModels<MyViewModel>() { factory }
+//        binding.textView.text = myViewModel.counter.toString()
 
-        binding.textView.text = myViewModel.counter.toString()
+        binding.lifecycleOwner = this
+        binding.viewmodel = myViewModel
+
+
         binding.button.setOnClickListener {
 //            myViewModel.counter += 1
 //            binding.textView.text = myViewModel.counter.toString()
 //            myViewModel.saveState()
+
             myViewModel.liveCounter.value = myViewModel.liveCounter.value?.plus(1)
         }
 
-        myViewModel.modifiedCounter.observe(this) { counter ->
-            binding.textView.text = counter.toString()
-        }
+//        myViewModel.liveCounter.observe(this) { counter ->
+//            binding.textView.text = counter.toString()
+//        }
+//
+//        myViewModel.modifiedCounter.observe(this) { counter ->
+//            binding.textView.text = counter
+//        }
     }
 }
--- app/src/main/java/com/example/applyviewmodel/MyViewModel.kt
+++ app/src/main/java/com/example/applyviewmodel/MyViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.*
 class MyViewModel(_counter : Int,private val savedStateHandle: SavedStateHandle): ViewModel() {
 
 //    var counter : Int = _counter
+    var counter = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: _counter
+
     var liveCounter : MutableLiveData<Int> = MutableLiveData(_counter)
     val modifiedCounter : LiveData<String> = Transformations.map(liveCounter) { counter ->
         "$counter 입니다"
     }
 
-    var counter = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: _counter
+    val hasChecked : MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+
 
     fun saveState(){
         savedStateHandle[SAVE_STATE_KEY] = counter
